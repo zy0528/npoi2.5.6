b0d89217f38fcc44b3a20a5ce125d72b3d11197c
zy <515185828@qq.com>
2023-12-29 13:52:03 +0800

    DATEDELTA

---
+++ main/SS/Formula/Functions/DatedElta.cs
@@ -0,0 +1,101 @@
+﻿using NPOI.SS.Formula.Eval;
+using NPOI.SS.UserModel;
+using NPOI.Util;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace NPOI.SS.Formula.Functions
+{
+    public class DatedElta : Var3or4ArgFunction
+    {
+        public override ValueEval Evaluate(int srcRowIndex, int srcColumnIndex, ValueEval datePara, ValueEval numPara, ValueEval unitPara, ValueEval formatPara)
+        {
+            string formalFormat = "yyyy-MM-dd HH:mm:ss";
+            string result = string.Empty;
+            try
+            {
+                string timeStr = TextFunction.EvaluateStringArg(datePara, srcRowIndex, srcColumnIndex);
+                string format = TextFunction.EvaluateStringArg(formatPara, srcRowIndex, srcColumnIndex);
+                string unit = TextFunction.EvaluateStringArg(unitPara, srcRowIndex, srcColumnIndex);
+                DateTime time0;
+                if (!string.IsNullOrWhiteSpace(format) && !format.Contains("D") && !format.Contains("Y") && !format.Contains("M"))
+                {
+                    time0 = DateTime.Parse(DateTime.Now.ToShortDateString() + " " + timeStr.Trim());
+                    format = formalFormat;
+                }
+                else
+                {
+                    time0 = TimeHelper.GetDateTime(srcRowIndex, srcColumnIndex, datePara);
+                }
+
+                if (string.IsNullOrWhiteSpace(format))
+                {
+                    format = formalFormat;
+                }
+                double d1 = NumericFunction.SingleOperandEvaluate(numPara, srcRowIndex, srcColumnIndex);
+                result = Evaluate(time0, d1, format, unit);
+            }
+            catch (EvaluationException e)
+            {
+                return e.GetErrorEval();
+            }
+            return new StringEval(result);
+        }
+
+        public override ValueEval Evaluate(int srcRowIndex, int srcColumnIndex, ValueEval datePara, ValueEval numPara, ValueEval unitPara)
+        {
+            string result = string.Empty;
+            try
+            {
+                DateTime time0 = TimeHelper.GetDateTime(srcRowIndex, srcColumnIndex, datePara);
+                double d1 = NumericFunction.SingleOperandEvaluate(numPara, srcRowIndex, srcColumnIndex);
+                string unit = TextFunction.EvaluateStringArg(unitPara, srcRowIndex, srcColumnIndex);
+                result = Evaluate(time0, d1, string.Empty, unit);
+            }
+            catch (EvaluationException e)
+            {
+                return e.GetErrorEval();
+            }
+            return new StringEval(result);
+        }
+
+        private string Evaluate(DateTime dateTime, double d1, string format, string unit)
+        {
+            if (string.IsNullOrWhiteSpace(unit))
+            {
+                dateTime = dateTime.AddDays(d1);
+            }
+            else if (unit == "y" || unit == "Y")
+            {
+                dateTime = dateTime.AddYears((int)d1);
+            }
+            else if (unit == "M")
+            {
+                dateTime = dateTime.AddMonths((int)d1);
+            }
+            else if (unit == "D" || unit == "d")
+            {
+                dateTime = dateTime.AddDays((int)d1);
+            }
+            else if (unit == "h" || unit == "H")
+            {
+                dateTime = dateTime.AddHours(d1);
+            }
+            else if (unit == "m")
+            {
+                dateTime = dateTime.AddMinutes(d1);
+            }
+            else if (unit == "s" || unit == "S")
+            {
+                dateTime = dateTime.AddSeconds(d1);
+            }
+
+            if (string.IsNullOrEmpty(format))
+            {
+                return dateTime.ToShortDateString();
+            }
+            return dateTime.ToString(format);
+        }
+    }
+}
---
+++ testcases/main/SS/Formula/Functions/TestDatedElta.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using NUnit.Framework;
+using NPOI.SS.Formula.Eval;
+using NPOI.SS.UserModel;
+using NPOI.SS.Formula.Functions;
+
+namespace TestCases.SS.Formula.Functions
+{
+    [TestFixture]
+    public class TestDatedElta
+    {
+        [Test]
+        public void Test3()
+        {
+            ValueEval datePara = new StringEval("16:18:44");
+            ValueEval numPara = new NumberEval(1);
+            ValueEval unitPara = new StringEval("H");
+            ValueEval formatPara = new StringEval("HH:mm:ss");
+            ValueEval val = invokeDatedElta(datePara, numPara, unitPara, formatPara);
+        }
+
+
+        [Test]
+        public void Test2()
+        {
+            ValueEval datePara = new StringEval("2022-11-17");
+            ValueEval numPara = new NumberEval(10);
+            ValueEval unitPara = new StringEval("y");
+            ValueEval formatPara = new StringEval("");
+            ValueEval val = invokeDatedElta(datePara, numPara, unitPara, formatPara);
+        }
+
+
+        [Test]
+        public void Test1()
+        {
+            ValueEval datePara = new StringEval("2022-11-17");
+            ValueEval numPara = new NumberEval(10);
+            ValueEval unitPara = new StringEval("y");
+            ValueEval formatPara = new StringEval("yyyy-MM-dd");
+            ValueEval val = invokeDatedElta(datePara, numPara, unitPara, formatPara);
+        }
+
+
+        private static NumberEval Convert(DateTime d)
+        {
+            return new NumberEval(DateUtil.GetExcelDate(d));
+        }
+        private static ValueEval invokeDatedElta(params ValueEval[] args)
+        {
+            return new DatedElta().Evaluate(args, -1, -1);
+        }
+
+
+    }
+}
